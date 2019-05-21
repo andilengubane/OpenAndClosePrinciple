b56d4445b73a993ed32ec39ca15a3d49a4bbd45b
andilengubane <mthembungubane@gmail.com>
2019-05-21 10:27:02 +0200

    open and close principle

---
+++ OpenAndClosePrinciple/JuniorDevSalaryCalculator.cs
@@ -0,0 +1,13 @@
+﻿namespace OpenAndClosePrinciple
+{
+    public class JuniorDevSalaryCalculator : BaseSalaryCalculator
+    {
+        public JuniorDevSalaryCalculator(DeveloperReport developerReport)
+            : base(developerReport)
+        {
+
+        }
+
+        public override double CalculateSalary() => DeveloperReport.HourlyRate * DeveloperReport.WorkingHours;
+    }
+}
---
+++ OpenAndClosePrinciple/SalaryCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace OpenAndClosePrinciple
+{
+    public class SalaryCalculator
+    {
+        private readonly IEnumerable<BaseSalaryCalculator> _developerCalculation;
+        public SalaryCalculator(IEnumerable<BaseSalaryCalculator> developerCalculation)
+        {
+            _developerCalculation = developerCalculation;
+        }
+
+        public double CalculateTotalSalaries()
+        {
+            double totalSalaries = 0D;
+
+            foreach (var devCalc in _developerCalculation)
+            {
+                totalSalaries += devCalc.CalculateSalary();
+            }
+
+            return totalSalaries;
+        }
+    }
+}
